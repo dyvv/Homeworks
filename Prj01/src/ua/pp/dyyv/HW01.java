b99a289090f48fe39d2948bd2d539461aa5700a5
dyvv <dyv@ukr.net>
2015-11-26 21:00:15 +0200

    First programm for debugging

--- Prj01/src/ua/pp/dyyv/HW01.java
+++ Prj01/src/ua/pp/dyyv/HW01.java
@@ -1,10 +1,29 @@
 package ua.pp.dyyv;
 
+import java.util.Scanner;
+
 public class HW01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Test01");
+		Scanner scan = new Scanner(System.in);
+		int a, b;
+		boolean bul=true;
+		System.out.println("This is a simple calculator.\n  Please enter the first number:");
+		beg: do  {
+			//System.out.println("You've entered incorrect number ");
+			if(scan.hasNextInt())
+			
+			{a = scan.nextInt();
+			 bul=false;  }
+			
+			else
+			{System.out.println("You've entered incorrect data...Please try again");
+			continue beg;}
+		} while (bul);
+			
+		}
+
 	}
 
-}
+
